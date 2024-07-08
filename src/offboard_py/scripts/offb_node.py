2f81fddee113ddf04b8dc56eeae9c021b0ac76d2
cagvanderaa <c.a.g.vanderaa@student.tudelft.nl>
2024-07-08 13:11:22 +0000

    Added multi vehicle sim and control

--- src/offboard_py/scripts/offb_node.py
+++ src/offboard_py/scripts/offb_node.py
@@ -20,23 +20,25 @@ def pose_cb(msg):
 if __name__ == "__main__":
     rospy.init_node("offb_node_py")
 
-    state_sub = rospy.Subscriber("mavros/state", State, callback=state_cb)
-    local_pos_sub = rospy.Subscriber("mavros/local_position/pose", PoseStamped, callback=pose_cb)
+    namespace = rospy.get_namespace()
+    rospy.loginfo(f"Namespace: {namespace}")
 
-    local_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+    state_sub = rospy.Subscriber(f"{namespace}mavros/state", State, callback=state_cb)
+    local_pos_sub = rospy.Subscriber(f"{namespace}mavros/local_position/pose", PoseStamped, callback=pose_cb)
 
-    rospy.wait_for_service("/mavros/cmd/arming")
-    arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
+    local_pos_pub = rospy.Publisher(f"{namespace}mavros/setpoint_position/local", PoseStamped, queue_size=10)
 
-    rospy.wait_for_service("/mavros/set_mode")
-    set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+    rospy.wait_for_service(f"{namespace}mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy(f"{namespace}mavros/cmd/arming", CommandBool)
 
+    rospy.wait_for_service(f"{namespace}mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy(f"{namespace}mavros/set_mode", SetMode)
 
     # Setpoint publishing MUST be faster than 2Hz
     rate = rospy.Rate(20)
 
     # Wait for Flight Controller connection
-    while(not rospy.is_shutdown() and not current_state.connected):
+    while not rospy.is_shutdown() and not current_state.connected:
         rate.sleep()
 
     pose = PoseStamped()
@@ -47,7 +49,7 @@ if __name__ == "__main__":
 
     # Send a few setpoints before starting
     for i in range(100):
-        if(rospy.is_shutdown()):
+        if rospy.is_shutdown():
             break
 
         local_pos_pub.publish(pose)
@@ -61,15 +63,15 @@ if __name__ == "__main__":
 
     last_req = rospy.Time.now()
 
-    while(not rospy.is_shutdown()):
-        if(current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-            if(set_mode_client.call(offb_set_mode).mode_sent == True):
+    while not rospy.is_shutdown():
+        if current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
+            if set_mode_client.call(offb_set_mode).mode_sent:
                 rospy.loginfo("OFFBOARD enabled")
 
             last_req = rospy.Time.now()
         else:
-            if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-                if(arming_client.call(arm_cmd).success == True):
+            if not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
+                if arming_client.call(arm_cmd).success:
                     rospy.loginfo("Vehicle armed")
 
                 last_req = rospy.Time.now()
